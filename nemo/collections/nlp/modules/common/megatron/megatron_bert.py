29ce11f7bde829ab4fa2b2bc9923024e136640ef
ericharper <complex451@gmail.com>
2020-11-18 22:03:03 -0700

    map location
    Signed-off-by: ericharper <complex451@gmail.com>

--- nemo/collections/nlp/modules/common/megatron/megatron_bert.py
+++ nemo/collections/nlp/modules/common/megatron/megatron_bert.py
@@ -138,7 +138,7 @@ class MegatronBertEncoder(BertModule):
         self._restore_path = restore_path
         if os.path.isfile(restore_path):
             logging.info(f'restore_path: {restore_path} is a file. Assuming no megatron model parallelism')
-            state_dict = torch.load(restore_path)
+            state_dict = torch.load(restore_path, map_location='cpu')
             # to load from Megatron pretrained checkpoint
             if 'model' in state_dict:
                 self.language_model.load_state_dict(state_dict['model'][self._language_model_key])
